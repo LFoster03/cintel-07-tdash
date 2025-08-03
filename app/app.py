aa74cd725d323c85cbe7346784ac371d76ba5e5a
LFoster03 <lindsay.foster03@gmail.com>
2025-08-03 09:48:25 -0500

    Update app.py
    changed page title

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ import palmerpenguins
 
 df = palmerpenguins.load_penguins()
 
-ui.page_opts(title="Penguins dashboard", fillable=True)
+ui.page_opts(title="Lindsay Foster's Penguins Dashboard", fillable=True)
 
 
 with ui.sidebar(title="Filter controls"):
